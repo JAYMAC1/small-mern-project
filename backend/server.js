4b324604f6c0f45da7c17200b8ff0f5726cb76e2
James McNeil <mcneil_james@hotmail.com>
2022-04-12 19:49:40 +0100

    added errorMidleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 const express = require('express') // imports express
 const dotenv = require('dotenv').config() // import dotenv for using environment variables
 
+const { errorHandler } = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 5000 // Set port for express app
 
 const app = express() // Initialise express as app
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: false })) // parse body where urlEncoded
 
 // add route
 app.use('/api/goals', require('./routes/goalRoutes'))
+app.use(errorHandler)
 
 // Set express app to listen on portfor requests
 app.listen(PORT, () => {
