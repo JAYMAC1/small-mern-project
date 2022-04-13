b20a711904afee2c7a31ec8b193fe9425dc8ee07
James McNeil <mcneil_james@hotmail.com>
2022-04-13 09:56:04 +0100

    set all controller functions to async

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,14 +1,14 @@
 // @desc    controller to fetch goals
 // @route   GET /api/goals
 // @access  Private
-const getGoals = (req, res) => {
+const getGoals = async (req, res) => {
   res.status(200).json({ message: 'GET goals' })
 }
 
 // @desc    controller to add goal
 // @route   POST /api/goals
 // @access  Private
-const setGoal = (req, res) => {
+const setGoal = async (req, res) => {
   if (!req.body.text) {
     res.status(400)
     throw new Error('Please add a text field')
@@ -19,14 +19,14 @@ const setGoal = (req, res) => {
 // @desc    controller to update goal based on id
 // @route   PUT /api/goals/:id
 // @access  Private
-const updateGoal = (req, res) => {
+const updateGoal = async (req, res) => {
   res.status(200).json({ message: `Update goal id: ${req.params.id}` })
 }
 
 // @desc    controller to delete goal based on id
 // @route   DELETE /api/goals/:id
 // @access  Private
-const deleteGoal = (req, res) => {
+const deleteGoal = async (req, res) => {
   res.status(200).json({ message: `deleting goal id: ${req.params.id}` })
 }
 
