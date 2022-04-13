c31a01f30335f431dda98b0209913fb54a93e3be
James McNeil <mcneil_james@hotmail.com>
2022-04-13 10:42:46 +0100

    created goal schema and set to export

---
+++ backend/model/goalModel.js
@@ -0,0 +1,16 @@
+// Define Schema for goals collection
+const mongoose = require('mongoose')
+
+const goalSchema = mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: [true, 'Please add a text value'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('Goal', goalSchema)
