84325a1adee1dded3cf9c2d152222fd5a0aa32ac
James McNeil <mcneil_james@hotmail.com>
2022-04-16 11:07:23 +0100

    removed the code to find user

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -37,16 +37,14 @@ const updateGoal = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('Goal not Found')
   }
-  const user = await User.findById(req.user.id)
-
   // Check for user
-  if (!user) {
+  if (!req.user) {
     res.status(401)
     throw new Error('User not found')
   }
 
   // Check if user is valid to update goal
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401)
     throw new Error('Not Authorised!')
   }
@@ -67,16 +65,14 @@ const deleteGoal = asyncHandler(async (req, res) => {
     throw new Error('Goal not Found')
   }
 
-  const user = await User.findById(req.user.id)
-
   // Check for user
-  if (!user) {
+  if (!req.user) {
     res.status(401)
     throw new Error('User not found')
   }
 
   // Check if user is valid to update goal
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401)
     throw new Error('Not Authorised!')
   }
